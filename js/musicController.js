26cbfad6482efd3339f2f934f8a0b37a82189a78
Julian Niebieskikwiat <juliannieb@gmail.com>
2017-10-23 00:56:20 -0500

    First version of MusicController class

--- js/musicController.js
+++ js/musicController.js
@@ -10,6 +10,23 @@ class MusicController {
     
     constructor(stages) {
         this.stages = stages;
+        this.audios = [];
+    }
+
+    createAudios() {
+        for (var i = 0; i < this.stages.length; i++) {
+            this.audios.push(newAudio(stages[i].audioSource));
+        }
+    }
+
+    newAudio(audioSource) {
+        return new Audio(audioSource);
+    }
+
+    play() {
+        for (var i = 0; i < this.audios.length; i++) {
+            this.audios[i].play();
+        }
     }
 
 }
