c8f088ddd35152ca494d76863b2e1e0274dd1757
Julian Niebieskikwiat <juliannieb@gmail.com>
2017-10-23 00:49:07 -0500

    Init MusicController class

---
+++ js/musicController.js
@@ -0,0 +1,15 @@
+/**
+ * @file
+ * This file provides a class to manage the music of the project.
+ */
+
+ /**
+ * Class representing a controller for the music of the project.
+ */
+class MusicController {
+    
+    constructor(stages) {
+        this.stages = stages;
+    }
+
+}
