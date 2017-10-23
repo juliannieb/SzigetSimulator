ebe867855a044392c3907967b6b1715c5962c5c3
Julian Niebieskikwiat <juliannieb@gmail.com>
2017-10-23 01:23:13 -0500

    Add distanceFrom method to Stage class

--- js/index.js
+++ js/index.js
@@ -4,8 +4,8 @@
  */
 
 $( document ).ready(function(){
-    let stage1 = new Stage("resources/TheXX_IDareYou.mp3");
-    let stage2 = new Stage("resources/TheKillers_ADustland Fairytale.mp3");
+    let stage1 = new Stage(-2, 1, "resources/TheXX_IDareYou.mp3");
+    let stage2 = new Stage(1, 5, "resources/TheKillers_ADustland Fairytale.mp3");
     let stages = [stage1, stage2];
     let musicController = new MusicController(stages);
     musicController.createAudios();
--- js/stage.js
+++ js/stage.js
@@ -8,8 +8,17 @@
  */
 class Stage {
 
-    constructor(audioSource) {
+    constructor(posX, posY, audioSource) {
+        this.posX = posX;
+        this.posY = posY;
         this.audioSource = audioSource;
+    }
+
+    distanceFrom(x, y) {
+        let distanceX = Math.abs(this.posX - x);
+        let distanceY = Math.abs(this.posY - y);
+        let distance = Math.sqrt(Math.pow(distanceX, 2) + Math.pow(distanceY, 2));
+        return distance;
     }
 
 }
